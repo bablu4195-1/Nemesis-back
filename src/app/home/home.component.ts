8aca8c2703031e5fe188f2881e563ca629c4ab2c
bablu4195 <bablu4195@gmail.com>
2021-02-17 22:29:30 +0530

    modified components

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+selector: 'app-home',
+templateUrl: './home.component.html',
+styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+products: any[] = [];
+constructor(private apiService: ApiService) { }
+// tslint:disable-next-line:typedef
+ngOnInit() {
+this.apiService.get().subscribe((data: any[]) => {
+console.log(data);
+this.products = data;
+});
+}
 }
